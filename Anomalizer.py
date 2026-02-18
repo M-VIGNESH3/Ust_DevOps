16aea3f482195e9bd3cef0140f0976f2e180bcea
M Vignesh <vigneshmtinku@gmail.com>
2026-02-18 16:25:29 +0530

    secound

--- Anomalizer.py
+++ Anomalizer.py
@@ -1,18 +1,35 @@
 from collections import Counter
 f=open('C:/Users/Admin/Desktop/temp/log.txt')
 l=f.read()
+
 f.close()
-l=l.split('\n')
-print(l)
+ll=l.split('\n')
+l=l.split()
+# print(l)
 
 # print(l)
 d={}
 for i in l:
-    t=i.split()
-    for j in t:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+s=sum(d.values())
+rl=[]
+print(s)
+for i in d.keys():
+    t=d[i]
+    
+    if round((t/s)*100,3) < 1:
         
-        if j not in d:
-            d[j]=1
-        else:
-            d[j]+=1
-print(d.values())
+        rl.append(i)
+
+print(d)
+print(rl)
+c=1
+for i in ll:
+    for j in rl:
+        if j in i:
+            print(c)
+    c+=1
+
